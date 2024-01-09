ade8772ffeb87c85825fa59c94536420bc9f1351
Halil <halillkesgin@gmail.com>
2024-01-09 23:41:04 +0300

    middleware done

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server"
 
 import * as z from "zod"
+import bcrypt from "bcrypt"
 
+import { db } from "@/lib/db"
 import { RegisterSchema } from "@/schemas"
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
@@ -10,6 +13,23 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return { error: "Invalid fields" }
     }
+    
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-    return { success: "Email sent" }
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+        return { error: "Email already in use" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    return { success: "User created" }
 }
